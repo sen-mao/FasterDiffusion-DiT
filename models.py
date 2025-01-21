7efcb53c316a94d6900d0304e5df3bd715b36fa4
Simon <senmaonk@gmail.com>
2025-01-21 09:23:27 +0800

    update

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ import math
 from timm.models.vision_transformer import PatchEmbed, Attention, Mlp
 
 
+
 def modulate(x, shift, scale):
     return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
 
